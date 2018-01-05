112778df70ab8fe6c8841c7edd8ee1d14c6d306d
saka <876038559@qq.com>
2018-01-05 14:56:10 +0800

    第一次运行

--- MysqlUTF.py
+++ MysqlUTF.py
@@ -1,5 +1,3 @@
-
-
 mysqld = "[mysqld]"
 client = "[client]"
 mysql = "[mysql]"
@@ -7,23 +5,46 @@ server_charset = "character-set-server=utf8"
 client_charset = "default-character-set=utf8"
 
 
+def backup_file(file_object, data):
+    print('back up your file to %s.bak_bak...' % file_object)
+    f = open(file_object.name + '.bak_bak', 'w')
+    name = f.name
+    f.writelines(data)
+    f.flush()
+    f.close()
+    print('back up your file success,you can open find it : %s' % name)
+
+
 def findMysqld(file_object, data):
     if data.find(mysqld) < 0:
+        backup_file(file_object, data)
+        print("prepare write into file...")
+        file_object.write(data)
         file_object.write('\n')
         file_object.write(mysqld)
         file_object.write('\n')
         file_object.writelines(server_charset)
+        file_object.flush()
+        print("write file success...")
+    elif data.find(server_charset) < 0:
+        backup_file(file_object,data)
+        print("prepare write into file...")
+        file_object.seek(0,0)
+        str = data.replace(mysqld, mysqld + '\n' + server_charset)
+        file_object.writelines(str)
+        file_object.flush()
+        print("write file success...")
     else:
-        print("you have already write server_char_set")
+        print("you have already write server_char_set...")
 
 
 def correct():
-    file_object = open('/etc/mysql/mysql.conf.d/mysqld.cnf', 'r+')
+    f = open('Test.txt', 'r+')
     try:
-        all_text = file_object.read()
-        findMysqld(file_object, all_text)
+        all_text = f.read()
+        findMysqld(f, all_text)
     finally:
-        file_object.close()
+        f.close()
 
 
 correct()
